cf3f697412e4ad888d8bb8646c05a84c57e9d641
Manuel <manuel.casanova.ucv@gmail.com>
2025-03-22 14:39:41 -0400

    Fin del eVentos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log ('Hola Mundo')
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+
+input.addEventListener( 'keyup' ,(e) => {
+            console.log(e.key);
+        
+})
+
+
+
+
+
